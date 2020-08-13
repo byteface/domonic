889fd711ccea7684da82daf411cd45d695b21c5a
byteface <byteface@googlemail.com>
2020-08-13 21:10:12 +0100

    bump.

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as f:
 
 setup(
   name = 'domonic',
-  version = '0.1.1',
+  version = '0.1.2',
   author="@byteface",
   author_email="byteface@gmail.com",
   license="MIT",
   url = 'https://github.com/byteface/domonic',
-  download_url = 'https://github.com/byteface/pypals/archive/0.1.0.tar.gz',
-  description = 'generate html with python 3',
+  download_url = 'https://github.com/byteface/pypals/archive/0.1.2.tar.gz',
+  description = 'generate html with python 3 and quite a bit more',
   long_description=long_description,
   long_description_content_type="text/markdown",
   keywords = ['html', 'generate', 'templating', 'dom', 'terminal'],
