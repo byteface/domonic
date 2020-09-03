f2883b08a036b1e5be8fa7202f5ed1a4fce87d6a
byteface <byteface@googlemail.com>
2020-09-03 20:28:19 +0100

    looks like that built

--- domonic/__init__.py
+++ domonic/__init__.py
@@ -7,7 +7,7 @@
     - Call Terminal commands using python 3 (this one requires a nix machine)
 """
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 __license__ = 'MIT'
 
 # from typing import *
--- setup.py
+++ setup.py
@@ -3,18 +3,21 @@ from setuptools import setup
 with open("README.md", "r") as f:
         long_description = f.read()
 
+version = '0.2.2'
+
 setup(
   name = 'domonic',
-  version = '0.2.1',
+  version = version,
   author="@byteface",
   author_email="byteface@gmail.com",
   license="MIT",
   url = 'https://github.com/byteface/domonic',
-  download_url = 'https://github.com/byteface/pypals/archive/0.2.1.tar.gz',
-  description = 'generate html with python 3 and quite a bit more',
+  download_url = 'https://github.com/byteface/pypals/archive/'+version+'.tar.gz',
+  description = 'Generate html with python 3. DOM API and more...',
   long_description=long_description,
   long_description_content_type="text/markdown",
-  keywords = ['html', 'generate', 'templating', 'dom', 'terminal', 'json', 'web', 'template', 'javascript', 'DOM', 'GUI'],
+  keywords = ['html', 'generate', 'templating', 'dom', 'terminal', 'json', 'web', 'template', 'javascript', 'DOM', 'GUI', 'render', 'website', 'apps', 'html5', 'template', 'framework'],
+  python_requires='>=3.6',
   classifiers=[
       "Programming Language :: Python :: 3",
       "Programming Language :: JavaScript",
@@ -35,7 +38,10 @@ setup(
       "Topic :: Software Development",
       "Topic :: Software Development :: Code Generators",
       "Topic :: Terminals",
-      "Topic :: Utilities"
+      "Topic :: Utilities",
+      'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+      'Topic :: Software Development :: Libraries :: Python Modules',
+      'Topic :: Text Processing :: Markup :: HTML',
   ],
   install_requires=[
           'requests', 'python-dateutil', 'urllib3'
