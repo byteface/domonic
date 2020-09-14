882f3b6fc39cf36f1f4bbc3e706bb6914432b85b
byteface <byteface@googlemail.com>
2020-09-15 00:48:41 +0100

    apparently works on next version which means they havent caught the double init bug im exploiting

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
       "Programming Language :: Python :: 3.6",
       "Programming Language :: Python :: 3.7",
       "Programming Language :: Python :: 3.8",
+      "Programming Language :: Python :: 3.9",
       "Development Status :: 4 - Beta",
       "Environment :: Web Environment",
       "Intended Audience :: Developers",
