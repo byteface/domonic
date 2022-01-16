e344c19e7f5f8858b2b0f0b78e0c7815c53355af
byteface <byteface@googlemail.com>
2022-01-16 12:45:37 +0000

    lots of this seems to just work

--- domonic/webapi/xpath.py
+++ domonic/webapi/xpath.py
@@ -3,6 +3,11 @@
     ====================================
     https://developer.mozilla.org/en-US/docs/Glossary/XPath
 
+    uses elementpath lib.
+
+    TODO - content strings must be TextNodes for it to work. 
+        so will have to iterate and update them. i.e. Treewalker
+
 """
 
 from typing import Any, Callable, Dict, List, Optional, Union
@@ -11,23 +16,31 @@ import json
 import os
 import sys
 
+import elementpath
+
 
 class XPathEvaluator:
 
     def __init__(self) -> None:
         pass
 
+    def createExpression(self, expression: str):  # , namespaces: Dict[str, str]) -> None:
+        return XPathExpression(expression)
+
 
 class XPathException:
-
     def __init__(self) -> None:
         pass
 
+class XPathExpression(object):
 
-class XPathExpression:
+    def __init__(self, expr: str):  #, resolver):
+        if len(expr) <= 0:
+            raise Exception('no expression')
+        self.selector = elementpath.Selector(expr)
 
-    def __init__(self) -> None:
-        pass
+    def evaluate(self, node, type=6):  # XPathResult.ANY_TYPE):
+        return XPathResult(self.selector.select(node), type)
 
 
 class XPathNSResolver:
@@ -49,8 +62,53 @@ class XPathResult:
     ANY_UNORDERED_NODE_TYPE = 8
     FIRST_ORDERED_NODE_TYPE = 9
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, value, _type):
+        if _type == XPathResult.ANY_TYPE:
+            tov = type(value)
+            if tov == 'object':
+                _type = self.UNORDERED_NODE_ITERATOR_TYPE
+            if tov == 'boolean':
+                _type = self.BOOLEAN_TYPE
+            if tov == 'string':
+                _type = self.STRING_TYPE
+            if tov == 'number':
+                _type = self.NUMBER_TYPE
 
-    # def snapshotItem(i):
-        # pass
+        if _type < self.NUMBER_TYPE or self.FIRST_ORDERED_NODE_TYPE < _type:
+            raise Exception(f'unknown type: {_type}')
+
+        self.resultType = _type
+
+        if _type == self.NUMBER_TYPE:
+            #self.numberValue=value.number() if getattr(value,'isNodeSet',None) else toNumber(value)
+            if getattr(value, 'isNodeSet', None):
+                self.numberValue = value#.number()
+            else:
+                self.numberValue = float(value)
+        elif _type == self.STRING_TYPE:
+            #self.stringValue=value.string() if getattr(value,'isNodeSet',None) else toString(value)
+            if getattr(value, 'isNodeSet', None):
+                self.stringValue = value#.string()
+            else:
+                self.stringValue = str(value)
+        elif _type == self.BOOLEAN_TYPE:
+            #self.booleanValue=value.bool() if getattr(value,'isNodeSet',None) else toBoolean(value)
+            if getattr(value, 'isNodeSet', None):
+                self.booleanValue = value#.bool()
+            else:
+                self.booleanValue = bool(value)
+        elif _type == self.ANY_UNORDERED_NODE_TYPE or _type == self.FIRST_ORDERED_NODE_TYPE:
+            self.singleNodeValue = value#.first()
+        else:
+            self.nodes = value#.list()
+            self.snapshotLength = len(value)
+            self.index = 0
+            self.invalidIteratorState = False
+
+    # def iterateNext(self):
+    #     node = self.nodes[self.index]
+    #     self.index += 1
+    #     return node
+
+    # def snapshotItem(self, i):
+    #     return self.nodes[i]
--- tests/test_webapi.py
+++ tests/test_webapi.py
@@ -145,5 +145,325 @@ class TestCase(unittest.TestCase):
         # print(f)
 
 
+    def test_xpath(self):
+
+        from domonic import domonic
+        from domonic.webapi.xpath import XPathEvaluator, XPathResult
+
+        # api unit test based on mdn example
+        # https://developer.mozilla.org/en-US/docs/Web/API/XPathEvaluator
+
+        somehtml = '''
+        <div>XPath example</div>
+        <div>Number of &lt;div&gt;s: <output></output></div>
+        '''
+        page = domonic.parseString(somehtml)  # NOTE - probably requries html5lib install
+        evaluator = XPathEvaluator()
+        expression = evaluator.createExpression("//div")
+        result = expression.evaluate(page, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE)
+        assert result.snapshotLength == 2
+        # print(result.nodes)
+
+        import requests
+        r = requests.get("http://eventual.technology")
+        page = domonic.parseString(r.content.decode("utf-8"))
+
+        ## Selectors
+
+        evaluator = XPathEvaluator()
+        expression = evaluator.createExpression("//h1")
+        result = expression.evaluate(page, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE)
+        assert str(result.nodes[0]) == '<h1 class="text-uppercase hero-text text-black">We are<span class="font-weight-bold d-block">Eventual Technology</span></h1>'
+
+        expression = evaluator.createExpression("//div//p")
+        result = expression.evaluate(page, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE)
+        assert str(result.nodes[0]) == '<p class="headings-font-family text-uppercase lead">Welcome to the information age</p>'
+
+        expression = evaluator.createExpression("//ul/li")
+        result = expression.evaluate(page, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE)
+        print(str(result.nodes))
+
+        expression = evaluator.createExpression("//ul/li/a")
+        result = expression.evaluate(page, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE)
+        print(str(result.nodes))
+
+        expression = evaluator.createExpression("//ul/li/a")
+        result = expression.evaluate(page, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE)
+        print(str(result.nodes))
+
+        expression = evaluator.createExpression("//ul/li/a")
+        result = expression.evaluate(page, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE)
+        print(str(result.nodes))
+
+        expression = evaluator.createExpression("//div/*")
+        result = expression.evaluate(page, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE)
+        print(str(result.nodes))
+
+        # root fails?
+        # expression = evaluator.createExpression("/")
+        # result = expression.evaluate(page, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE)
+        # print(str(result.nodes[0]))
+
+        # expression = evaluator.createExpression("/body")
+        # result = expression.evaluate(page, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE)
+        # print(str(result.nodes[0]))
+
+        # NOTE - attributes reqiures underscores
+        expression = evaluator.createExpression('//*[@_id="contact"]')
+        result = expression.evaluate(page, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE)
+        print(str(result.nodes))
+
+        expression = evaluator.createExpression('//*[@_class="social-link social-link-instagram"]')  # NOTE - requires all classes to match
+        result = expression.evaluate(page, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE)
+        print(str(result.nodes))
+
+        # expression = evaluator.createExpression("//input[@type='submit']")
+        # result = expression.evaluate(page, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE)
+        # print(str(result.nodes))
+
+        expression = evaluator.createExpression("//a[contains(@_href, 'twitter')]")
+        result = expression.evaluate(page, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE)
+        print(str(result.nodes[0]))
+        # wow. nice!
+
+        expression = evaluator.createExpression('//*[last()][name()="a"]')
+        result = expression.evaluate(page, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE)
+        print(str(result.nodes[0]))
+        # so cool this all works out of the box!
+
+        expression = evaluator.createExpression('//span/text()')
+        result = expression.evaluate(page, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE)
+        print(str(result.nodes[0]))
+
+        '''
+        TODO - unit tests for the following so i know what works
+
+        Descendant selectors
+        h1	//h1	?
+        div p	//div//p	?
+        ul > li	//ul/li	?
+        ul > li > a	//ul/li/a
+        div > *	//div/*
+        :root	/	?
+        :root > body	/body
+
+        Attribute selectors
+        #id	//*[@id="id"]	?
+        .class	//*[@class="class"] …kinda
+        input[type="submit"]	//input[@type="submit"]
+        a#abc[for="xyz"]	//a[@id="abc"][@for="xyz"]	?
+        a[rel]	//a[@rel]
+        a[href^='/']	//a[starts-with(@href, '/')]	?
+        a[href$='pdf']	//a[ends-with(@href, '.pdf')]
+        a[href*='://']	//a[contains(@href, '://')]
+        a[rel~='help']	//a[contains(@rel, 'help')] …kinda
+
+        Order selectors
+        ul > li:first-of-type	//ul/li[1]	?
+        ul > li:nth-of-type(2)	//ul/li[2]
+        ul > li:last-of-type	//ul/li[last()]
+        li#id:first-of-type	//li[1][@id="id"]	?
+        a:first-child	//*[1][name()="a"]
+        a:last-child	//*[last()][name()="a"]
+
+        Siblings
+        h1 ~ ul	//h1/following-sibling::ul	?
+        h1 + ul	//h1/following-sibling::ul[1]
+        h1 ~ #id	//h1/following-sibling::[@id="id"]
+
+        jQuery
+        $('ul > li').parent()	//ul/li/..	?
+        $('li').closest('section')	//li/ancestor-or-self::section
+        $('a').attr('href')	//a/@href	?
+        $('span').text()	//span/text()
+
+        Other things
+        h1:not([id])	//h1[not(@id)]	?
+
+        Text match	//button[text()="Submit"]	?
+        Text match (substring)	//button[contains(text(),"Go")]
+
+        Arithmetic	//product[@price > 2.50]
+
+        Has children	//ul[*]
+        Has children (specific)	//ul[li]
+        Or logic	//a[@name or @href]	?
+        Union (joins results)	//a | //div	?
+        Class check
+        //div[contains(concat(' ',normalize-space(@class),' '),' foobar ')]
+        Xpath doesn’t have the “check if part of space-separated list” operator, so this is the workaround (source).
+
+        #Expressions
+        Steps and axes
+        //	ul	/	a[@id='link']
+        Axis	Step	Axis	Step
+        Prefixes
+        Prefix	Example	What
+        //	//hr[@class='edge']	Anywhere
+        ./	./a	Relative
+        /	/html/body/div	Root
+        Begin your expression with any of these.
+
+        Axes
+        Axis	Example	What
+        /	//ul/li/a	Child
+        //	//[@id="list"]//a	Descendant
+        Separate your steps with /. Use two (//) if you don’t want to select direct children.
+
+        Steps
+        //div
+        //div[@name='box']
+        //[@id='link']
+        A step may have an element name (div) and predicates ([...]). Both are optional. They can also be these other things:
+
+        //a/text()     #=> "Go home"
+        //a/@href      #=> "index.html"
+        //a/*          #=> All a's child elements
+        #Predicates
+        Predicates
+        //div[true()]
+        //div[@class="head"]
+        //div[@class="head"][@id="top"]
+        Restricts a nodeset only if some condition is true. They can be chained.
+
+        Operators
+        # Comparison
+        //a[@id = "xyz"]
+        //a[@id != "xyz"]
+        //a[@price > 25]
+        # Logic (and/or)
+        //div[@id="head" and position()=2]
+        //div[(x and y) or not(z)]
+        Use comparison and logic operators to make conditionals.
+
+        Using nodes
+        # Use them inside functions
+        //ul[count(li) > 2]
+        //ul[count(li[@class='hide']) > 0]
+        # This returns `<ul>` that has a `<li>` child
+        //ul[li]
+        You can use nodes inside predicates.
+
+        Indexing
+        //a[1]                  # first <a>
+        //a[last()]             # last <a>
+        //ol/li[2]              # second <li>
+        //ol/li[position()=2]   # same as above
+        //ol/li[position()>1]   # :not(:first-of-type)
+        Use [] with a number, or last() or position().
+
+        Chaining order
+        a[1][@href='/']
+        a[@href='/'][1]
+        Order is significant, these two are different.
+
+        Nesting predicates
+        //section[.//h1[@id='hi']]
+        This returns <section> if it has an <h1> descendant with id='hi'.
+
+        #Functions
+        Node functions
+        name()                     # //[starts-with(name(), 'h')]
+        text()                     # //button[text()="Submit"]
+                                # //button/text()
+        lang(str)
+        namespace-uri()
+        count()                    # //table[count(tr)=1]
+        position()                 # //ol/li[position()=2]
+        Boolean functions
+        not(expr)                  # button[not(starts-with(text(),"Submit"))]
+        String functions
+        contains()                 # font[contains(@class,"head")]
+        starts-with()              # font[starts-with(@class,"head")]
+        ends-with()                # font[ends-with(@class,"head")]
+        concat(x,y)
+        substring(str, start, len)
+        substring-before("01/02", "/")  #=> 01
+        substring-after("01/02", "/")   #=> 02
+        translate()
+        normalize-space()
+        string-length()
+        Type conversion
+        string()
+        number()
+        boolean()
+        #Axes
+        Using axes
+        //ul/li                       # ul > li
+        //ul/child::li                # ul > li (same)
+        //ul/following-sibling::li    # ul ~ li
+        //ul/descendant-or-self::li   # ul li
+        //ul/ancestor-or-self::li     # $('ul').closest('li')
+        Steps of an expression are separated by /, usually used to pick child nodes. That’s not always true: you can specify a different “axis” with ::.
+
+        //	ul	/child::	li
+        Axis	Step	Axis	Step
+        Child axis
+        # both the same
+        //ul/li/a
+        //child::ul/child::li/child::a
+        child:: is the default axis. This makes //a/b/c work.
+
+        # both the same
+        # this works because `child::li` is truthy, so the predicate succeeds
+        //ul[li]
+        //ul[child::li]
+        # both the same
+        //ul[count(li) > 2]
+        //ul[count(child::li) > 2]
+        Descendant-or-self axis
+        # both the same
+        //div//h4
+        //div/descendant-or-self::h4
+        // is short for the descendant-or-self:: axis.
+
+        # both the same
+        //ul//[last()]
+        //ul/descendant-or-self::[last()]
+        Other axes
+        Axis	Abbrev	Notes
+        ancestor
+        ancestor-or-self
+        attribute	@	@href is short for attribute::href
+        child	div is short for child::div
+        descendant
+        descendant-or-self	//	// is short for /descendant-or-self::node()/
+        namespace
+        self	.	. is short for self::node()
+        parent	..	.. is short for parent::node()
+        following
+        following-sibling
+        preceding
+        preceding-sibling
+        There are other axes you can use.
+
+        Unions
+        //a | //span
+        Use | to join two expressions.
+
+        #More examples
+        Examples
+        //*                 # all elements
+        count(//*)          # count all elements
+        (//h1)[1]/text()    # text of the first h1 heading
+        //li[span]          # find a <li> with an <span> inside it
+                            # ...expands to //li[child::span]
+        //ul/li/..          # use .. to select a parent
+        Find a parent
+        //section[h1[@id='section-name']]
+        Finds a <section> that directly contains h1#section-name
+
+        //section[//h1[@id='section-name']]
+        Finds a <section> that contains h1#section-name. (Same as above, but uses descendant-or-self instead of child)
+
+        Closest
+        ./ancestor-or-self::[@class="box"]
+        Works like jQuery’s $().closest('.box').
+
+        Attributes
+        //item[@price > 2*@discount]
+        '''
+
+
 if __name__ == '__main__':
     unittest.main()
