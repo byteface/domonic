3968c8bb59ac9177fd226fc3f407ffc6a04d4ba0
byteface <byteface@googlemail.com>
2022-03-24 23:56:51 +0000

    Adds Ukranian language tags

---
+++ domonic/i18n/uk/__init__.py
@@ -0,0 +1,90 @@
+"""
+    domonic.i18n.uk
+    ====================================
+
+    html tags in Ukrainian
+
+    first translated to Ukranian using google translate.
+    send a pull request if you have a better translation.
+"""
+from domonic.html import *
+
+tilo = тіло = body
+holova = голова = head
+stsenariy = сценарій = script
+# stylʹ = стиль = style
+portal = портал = portal
+# sylʹnyy = сильний = strong
+# blokova tsytata = блокова цитата = blockquote
+stil = стіл = table
+formu = форму = form
+etykettsi = етикетці = label
+podaty = подати = submit
+tytul = титул = title
+# noscript = noscript = noscript
+rozdil = розділ = section
+nav = нав = nav
+statti = статті = article
+# ostoronʹ = осторонь = aside
+# hgroup = hgroup = hgroup
+adresa = адреса = address
+fihura = фігура = figure
+# figcaption = figcaption = figcaption
+malyy = малий = small
+tsytuvaty = цитувати = cite
+kod = код = code
+pid = під = sub
+sup = суп = sup
+poznachka = позначка = mark
+rubinovyy = рубіновий = ruby
+promizhok = проміжок = span
+# iframe = iframe = iframe
+video = відео = video
+audio = аудіо = audio
+polotno = полотно = canvas
+pidpys = підпис = caption
+kolhrupa = колгрупа = colgroup
+# tbody = tbody = tbody
+# thead = thead = thead
+tfut = тфут = tfoot
+# nabir poliv = набір полів = fieldset
+lehenda = легенда = legend
+knopku = кнопку = button
+# vyberitʹ = виберіть = select
+# spysok danykh = список даних = datalist
+# optgroup = optgroup = optgroup
+variant = варіант = option
+# tekstova oblastʹ = текстова область = textarea
+vykhid = вихід = output
+prohres = прогрес = progress
+metr = метр = meter
+detali = деталі = details
+rezyume = резюме = summary
+menyu = меню = menu
+# punkt menyu = пункт меню = menuitem
+zaholovok = заголовок = header
+# nyzhniy kolontytul = нижній колонтитул = footer
+bazy = бази = base
+posylannya = посилання = link
+# meta = мета = meta
+# img = img = img
+vbudovuvaty = вбудовувати = embed
+dzherelo = джерело = source
+trek = трек = track
+ploshcha = площа = area
+kol = кол = col
+vvedennya = введення = input
+komanda = команда = command
+osnovnyy = основний = main
+# bazovyy shryft = базовий шрифт = basefont
+tsentr = центр = center
+vbudovuvaty = вбудовувати = embed
+shryft = шрифт = font
+# isindex = isindex = isindex
+perelik = перелік = listing
+# prostyy tekst = простий текст = plaintext
+strayk = страйк = strike
+shablon = шаблон = template
+kartyna = картина = picture
+doctype = doctype = doctype
+komentar = коментар = comment
