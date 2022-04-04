13ad147863001a9617c3936c3b56ab7bbef3bbba
byteface <byteface@googlemail.com>
2022-04-05 00:33:25 +0100

    Stops testing for 3.6

--- setup.py
+++ setup.py
@@ -61,7 +61,6 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: JavaScript",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
