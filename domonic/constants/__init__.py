c1117de95091f18b6f848d22bbd5acbf1c6d6b3e
byteface <byteface@googlemail.com>
2025-04-05 09:21:30 +0100

    Update __init__.py using Github Copilot advice on what to improve in this file

--- domonic/constants/__init__.py
+++ domonic/constants/__init__.py
@@ -1,11 +1,11 @@
 """
-    domonic.constants
-    ====================================
-
+domonic.constants
+====================================
+This module defines various constants used in the domonic package.
 """
 
-#: namespaces
-namespaces: dict = {
+# Namespaces
+namespaces: dict[str, str] = {
     "xml": "http://www.w3.org/XML/1998/namespace",
     "svg": "http://www.w3.org/2000/svg",
     "xlink": "http://www.w3.org/1999/xlink",
@@ -19,17 +19,9 @@ namespaces: dict = {
     "html": "http://www.w3.org/1999/xhtml",
 }
 
-
-#: document types
-doctypes: dict = {
+# Document Types
+doctypes: dict[str, str] = {
     "HTML5": "<!DOCTYPE html>",
-    # XHTML 1.1	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-    # XHTML 1.1 Strict	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-    # XHTML 1.1 Transitional	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-    # XHTML 1.1 Frameset	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
-    # XHTML 1.0 Strict	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-    # XHTML 1.0 Transitional	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-    # XHTML_1_0_Frameset = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">'
     "HTML4_01_Strict": '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">',
     "HTML4_01_Transitional": '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">',
     "HTML4_01_Frameset": '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">',
@@ -37,126 +29,115 @@ doctypes: dict = {
     "HTML2": '<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">',
 }
 
+# HTTP Response Status Codes
+from enum import Enum
 
-#: html response status codes
-http_response_status_codes: dict = {
-    200: "200 OK",
-    201: "201 Created",
-    202: "202 Accepted",
-    203: "203 Non-Authoritative Information",
-    204: "204 No Content",
-    205: "205 Reset Content",
-    206: "206 Partial Content",
-    207: "207 Multi-Status",
-    208: "208 Already Reported",
-    226: "226 IM Used",
-    300: "300 Multiple Choices",
-    301: "301 Moved Permanently",
-    302: "302 Found",
-    303: "303 See Other",
-    304: "304 Not Modified",
-    305: "305 Use Proxy",
-    306: "306 Switch Proxy",
-    307: "307 Temporary Redirect",
-    308: "308 Permanent Redirect",
-    400: "400 Bad Request",
-    401: "401 Unauthorized",
-    402: "402 Payment Required",
-    403: "403 Forbidden",
-    404: "404 Not Found",
-    405: "405 Method Not Allowed",
-    406: "406 Not Acceptable",
-    407: "407 Proxy Authentication Required",
-    408: "408 Request Timeout",
-    409: "409 Conflict",
-    410: "410 Gone",
-    411: "411 Length Required",
-    412: "412 Precondition Failed",
-    413: "413 Payload Too Large",
-    414: "414 URI Too Long",
-    415: "415 Unsupported Media Type",
-    416: "416 Range Not Satisfiable",
-    417: "417 Expectation Failed",
-    418: "418 I'm a teapot",
-    421: "421 Misdirected Request",
-    422: "422 Unprocessable Entity",
-    423: "423 Locked",
-    424: "424 Failed Dependency",
-    426: "426 Upgrade Required",
-    428: "428 Precondition Required",
-    429: "429 Too Many Requests",
-    431: "431 Request Header Fields Too Large",
-    451: "451 Unavailable For Legal Reasons",
-    499: "499 Client Closed Request",
-    500: "500 Internal Server Error",
-    501: "501 Not Implemented",
-    502: "502 Bad Gateway",
-    503: "503 Service Unavailable",
-    504: "504 Gateway Timeout",
-    505: "505 HTTP Version Not Supported",
-    506: "506 Variant Also Negotiates",
-    507: "507 Insufficient Storage",
-    508: "508 Loop Detected",
-    510: "510 Not Extended",
-    511: "511 Network Authentication Required",
+class HTTPStatus(Enum):
+    OK = 200
+    CREATED = 201
+    ACCEPTED = 202
+    NON_AUTHORITATIVE_INFORMATION = 203
+    NO_CONTENT = 204
+    RESET_CONTENT = 205
+    PARTIAL_CONTENT = 206
+    MULTI_STATUS = 207
+    ALREADY_REPORTED = 208
+    IM_USED = 226
+    MULTIPLE_CHOICES = 300
+    MOVED_PERMANENTLY = 301
+    FOUND = 302
+    SEE_OTHER = 303
+    NOT_MODIFIED = 304
+    USE_PROXY = 305
+    SWITCH_PROXY = 306
+    TEMPORARY_REDIRECT = 307
+    PERMANENT_REDIRECT = 308
+    BAD_REQUEST = 400
+    UNAUTHORIZED = 401
+    PAYMENT_REQUIRED = 402
+    FORBIDDEN = 403
+    NOT_FOUND = 404
+    METHOD_NOT_ALLOWED = 405
+    NOT_ACCEPTABLE = 406
+    PROXY_AUTHENTICATION_REQUIRED = 407
+    REQUEST_TIMEOUT = 408
+    CONFLICT = 409
+    GONE = 410
+    LENGTH_REQUIRED = 411
+    PRECONDITION_FAILED = 412
+    PAYLOAD_TOO_LARGE = 413
+    URI_TOO_LONG = 414
+    UNSUPPORTED_MEDIA_TYPE = 415
+    RANGE_NOT_SATISFIABLE = 416
+    EXPECTATION_FAILED = 417
+    IM_A_TEAPOT = 418
+    MISDIRECTED_REQUEST = 421
+    UNPROCESSABLE_ENTITY = 422
+    LOCKED = 423
+    FAILED_DEPENDENCY = 424
+    UPGRADE_REQUIRED = 426
+    PRECONDITION_REQUIRED = 428
+    TOO_MANY_REQUESTS = 429
+    REQUEST_HEADER_FIELDS_TOO_LARGE = 431
+    UNAVAILABLE_FOR_LEGAL_REASONS = 451
+    CLIENT_CLOSED_REQUEST = 499
+    INTERNAL_SERVER_ERROR = 500
+    NOT_IMPLEMENTED = 501
+    BAD_GATEWAY = 502
+    SERVICE_UNAVAILABLE = 503
+    GATEWAY_TIMEOUT = 504
+    HTTP_VERSION_NOT_SUPPORTED = 505
+    VARIANT_ALSO_NEGOTIATES = 506
+    INSUFFICIENT_STORAGE = 507
+    LOOP_DETECTED = 508
+    NOT_EXTENDED = 510
+    NETWORK_AUTHENTICATION_REQUIRED = 511
+
+http_response_status_codes = {status.value: status.name.replace('_', ' ').title() for status in HTTPStatus}
+
+# Common MIME Types
+file_extensions: dict[str, str] = {
+    "html": "text/html",
+    "htm": "text/html",
+    "xhtml": "application/xhtml+xml",
+    "xml": "application/xml",
+    "svg": "image/svg+xml",
+    "css": "text/css",
+    "js": "application/javascript",
+    "json": "application/json",
+    "txt": "text/plain",
+    "pdf": "application/pdf",
+    "png": "image/png",
+    "jpg": "image/jpeg",
+    "jpeg": "image/jpeg",
+    "gif": "image/gif",
+    "ico": "image/x-icon",
+    "tiff": "image/tiff",
+    "tif": "image/tiff",
+    "bmp": "image/bmp",
+    "mp3": "audio/mpeg",
+    "mp4": "video/mp4",
+    "mpeg": "video/mpeg",
+    "mpg": "video/mpeg",
+    "mov": "video/quicktime",
+    "qt": "video/quicktime",
+    "avi": "video/x-msvideo",
+    "wmv": "video/x-ms-wmv",
+    "flv": "video/x-flv",
+    "swf": "application/x-shockwave-flash",
+    "zip": "application/zip",
+    "gz": "application/x-gzip",
+    "bz2": "application/x-bzip2",
+    "rar": "application/x-rar-compressed",
+    "tar": "application/x-tar",
+    "7z": "application/x-7z-compressed",
+    "exe": "application/x-msdownload",
+    "msi": "application/x-msdownload",
+    "cab": "application/vnd.ms-cab-compressed",
+    "doc": "application/msword",
+    "docx": "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    "xls": "application/vnd.ms-excel",
+    "xlsx": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    "ppt": "application/vnd.ms-powerpoint",
+    "pptx": "application/vnd.openxmlformats-officedocument.presentationml.presentation",
 }
-
-
-# file_extensions = {
-#     "html": "text/html",
-#     "htm": "text/html",
-#     "xhtml": "application/xhtml+xml",
-#     "xml": "application/xml",
-#     "svg": "image/svg+xml",
-#     "css": "text/css",
-#     "js": "application/javascript",
-#     "json": "application/json",
-#     "txt": "text/plain",
-#     "pdf": "application/pdf",
-#     "png": "image/png",
-#     "jpg": "image/jpeg",
-#     "jpeg": "image/jpeg",
-#     "gif": "image/gif",
-#     "ico": "image/x-icon",
-#     "tiff": "image/tiff",
-#     "tif": "image/tiff",
-#     "bmp": "image/bmp",
-#     "mp3": "audio/mpeg",
-#     "mp4": "video/mp4",
-#     "mpeg": "video/mpeg",
-#     "mpg": "video/mpeg",
-#     "mov": "video/quicktime",
-#     "qt": "video/quicktime",
-#     "avi": "video/x-msvideo",
-#     "wmv": "video/x-ms-wmv",
-#     "flv": "video/x-flv",
-#     "swf": "application/x-shockwave-flash",
-#     "zip": "application/zip",
-#     "gz": "application/x-gzip",
-#     "bz2": "application/x-bzip2",
-#     "rar": "application/x-rar-compressed",
-#     "tar": "application/x-tar",
-#     "7z": "application/x-7z-compressed",
-#     "exe": "application/x-msdownload",
-#     "msi": "application/x-msdownload",
-#     "cab": "application/vnd.ms-cab-compressed",
-#     "doc": "application/msword",
-#     "docx": "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-#     "xls": "application/vnd.ms-excel",
-#     "xlsx": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-#     "ppt": "application/vnd.ms-powerpoint",
-#     "pptx": "application/vnd.openxmlformats-officedocument.presentationml.presentation",
-# }
-
-# """
-# tag_states = {
-#     'a' : {
-#         'hover' : 'a:hover',
-#         'visited' : 'a:visited',
-#         'link' : 'a:link',
-#         'active' : 'a:active',
-#         'focus' : 'a:focus',
-#         # 'not' : 'a:not',
-#     }
-# }
-# """
