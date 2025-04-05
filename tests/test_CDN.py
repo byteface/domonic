22e8a8c953ff2c86b53dc65fec80470ad1e50aae
byteface <byteface@googlemail.com>
2025-04-05 10:15:08 +0100

    Update test_CDN.py refactor test

--- tests/test_CDN.py
+++ tests/test_CDN.py
@@ -12,12 +12,10 @@ from domonic.html import img, link, script
 
 class TestCase(unittest.TestCase):
     def test_domonic_CDN(self):
-        myjs = script(_src=CDN_JS.JQUERY_3_5_1)
-        assert str(myjs) == '<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>'
+        myjs = script(_src=CDN_JS.JQUERY)
+        assert str(myjs) == '<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>'
         mycss = link(_href=CDN_CSS.MARX)
         assert str(mycss) == '<link href="https://unpkg.com/marx-css/css/marx.min.css"/>'
-        mycss = link(_rel="stylesheet", _href=CDN_JS.JQUERY_3_5_1)
-        assert str(mycss) == '<link rel="stylesheet" href="https://code.jquery.com/jquery-3.5.1.min.js"/>'
         myimg = img(_src=CDN_IMG.PLACEHOLDER(100, 100))
         assert str(myimg) == '<img src="://loremflickr.com/100/100"/>'
 
